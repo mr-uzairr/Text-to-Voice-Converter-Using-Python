b8efedb29fe4fa1e1d37ddb629e279a3bb6962c0
Uzair <uzairuzair050@gmail.com>
2025-04-01 21:39:12 +0500

    Text To speech Converter Using python

--- texttospeech.py
+++ texttospeech.py
@@ -8,10 +8,11 @@ text = abc.read() # file reading
 language = 'en' # en for english language
 
 obj = gTTS(text = text,lang = language, slow = False)
-
 # used slow = false because our converted video have a high speed
 
+
 obj.save("sample.mp3")
+
 
 os.system("open sample.mp3")
 
